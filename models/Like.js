cc5b9333203b19a25d65d904f5ef77362f9ef975
Tye Stanley <tyestanley090@gmail.com>
2022-06-24 17:27:45 -0500

    updated like model

--- models/Like.js
+++ models/Like.js
@@ -4,5 +4,32 @@ const sequelize = require('../config/connection');
 class Like extends Model {}
 
 Like.init(
-  
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'like'
+  }
 );
