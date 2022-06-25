241ff28db05c31cf15556b9326505355d4bdb527
CMiller <mlr.chase40@gmail.com>
2022-06-24 22:39:24 -0500

    adding routes

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+//router setup
+const router = require('express').Router();
+
+// connect to routes
+const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes');
+
+// route paths
+// router.use('/', homeRoutes);
